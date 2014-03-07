ce7b0ce8fc4d3d8073eb97525181b5b2c4040bf9
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-07 15:33:13 -0800

    Draw enemies, make enemies move.

--- watchout.js
+++ watchout.js
@@ -1,2 +1,45 @@
-// start slingin' some d3 here.
+var width = 960;
+var height = 500;
 
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var enemies = d3.range(10);
+
+
+var move = function(data){
+  var bad = svg.selectAll("circle")
+    .data(data, function(d){return d;});
+
+  bad.enter().append('circle')
+    .attr('r', function (){
+      return Math.random() * 20 +10;
+    })
+    .attr('cx', function(){
+      return Math.random() * width;
+    })
+    .attr('cy', function(){
+      return Math.random() * height;
+    });
+
+  //UPDATE
+  bad.transition()
+    .duration(1000)
+    .attr('r', function (){
+      return Math.random() * 20 +10;
+    })
+    .attr('cx', function(){
+      return Math.random() * width;
+    })
+    .attr('cy', function(){
+      return Math.random() * height;
+    });
+
+
+
+};
+
+setInterval(function(){
+  move(enemies);
+},1500);
