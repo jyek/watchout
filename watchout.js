874e0a4588a5d8cbb760830b3555c9dcc2bc8fd8
Nicholas Van de walle <nicholas@niceguys.co>
2014-03-07 19:29:00 -0800

    Collision detection added and verified correct count.

--- watchout.js
+++ watchout.js
@@ -5,7 +5,14 @@ var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var enemies = d3.range(10);
+var numEnemies = 4;
+var enemies = [];
+
+for(var i = 0; i < numEnemies; i++){
+  enemies.push({
+    t:false
+  });
+}
 
 var player = [{
   x: width / 2,
@@ -13,7 +20,7 @@ var player = [{
 }];
 
 var init = function(data){
-  var good = svg.selectAll("circle")
+  var good = svg.selectAll(".hero")
     .data(data, function(d){return d;});
 
   good.enter().append('circle')
@@ -26,6 +33,7 @@ var init = function(data){
     .attr('cy', function(d){
       return d.y;
     })
+    .attr('class', 'hero')
     .style("fill", "red")
     .call(d3.behavior.drag()
       .origin(function(d) { return d; })
@@ -37,8 +45,8 @@ var init = function(data){
 };
 
 var move = function(data){
-  var bad = svg.selectAll("circle")
-    .data(data, function(d){return d;});
+  var bad = svg.selectAll(".enemy")
+    .data(data);
 
   bad.enter().append('circle')
     .attr('r', function (){
@@ -49,7 +57,8 @@ var move = function(data){
     })
     .attr('cy', function(){
       return Math.random() * height;
-    });
+    })
+    .attr('class', 'enemy');
 
   //UPDATE
   bad.transition()
@@ -64,12 +73,45 @@ var move = function(data){
       return Math.random() * height;
     });
 
+};
 
+var distance = function(x1,y1,x2,y2){
+  return Math.sqrt(Math.pow(x1 - x2,2) + Math.pow(y1 - y2, 2));
+};
 
+var checkCollision = function(){
+  var hero = d3.select(".hero");
+  var enemyPosition = d3.selectAll(".enemy");
+  var x1 = hero.attr("cx");
+  var y1 = hero.attr("cy");
+  var r1 = parseInt(hero.attr("r"));
+  enemyPosition.each(function(d){
+    var x2 = d3.select(this).attr("cx");
+    var y2 = d3.select(this).attr("cy");
+    var r2 = parseInt(d3.select(this).attr("r"));
+    if(r1 + r2 > distance(x1,y1,x2,y2)){
+      // in contact
+
+      if(d.t === false){
+        //collisions +=1
+        d.t = true;
+      console.log("COLLISION!!");
+      }
+
+    }else{
+      //not in contact
+      d.t = false;
+    }
+  });
 };
 
 init(player);
 move(enemies);
+
+
+setInterval (function(){
+  checkCollision();
+},10);
 
 setInterval(function(){
   move(enemies);
